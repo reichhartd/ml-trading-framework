055a9d97ee180bbe45c7a92a3f08be950e99c1c0
Daniel Reichhart <daniel.reichhart@gmx.net>
2025-04-02 21:15:41 +0200

    feat: draft

--- src/data/kaggle_downloader.py
+++ src/data/kaggle_downloader.py
@@ -1,6 +1,11 @@
 import kagglehub
+import os
+from src.config import DATA_RAW_DIR
 
-# Download latest version
-path = kagglehub.dataset_download("mczielinski/bitcoin-historical-data")
+# Make sure target directory exists
+os.makedirs(DATA_RAW_DIR, exist_ok=True)
 
-print("Path to dataset files:", path)
+# Download directly to DATA_RAW_DIR
+path = kagglehub.dataset_download("mczielinski/bitcoin-historical-data", path=DATA_RAW_DIR)
+
+print(f"Dataset downloaded to: {path}")
