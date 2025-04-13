622625216a6ee3e04ff10c224f5727d217e4034f
Daniel Reichhart <daniel.reichhart@gmx.net>
2025-04-13 14:31:14 +0200

    feat: draft

--- src/data/kaggle_downloader.py
+++ src/data/kaggle_downloader.py
@@ -1,69 +1,26 @@
 import os
-import shutil
-import pandas as pd
-
 from kaggle.api.kaggle_api_extended import KaggleApi
 from zipfile import ZipFile
 
 
-class Dataset:
-    """
-    This class is a convenience wrapper for dataset functions. It taks care of interaction with the file system and downloading the dataset.
+ZIP_PATH = "data/zip/"
+ZIP_NAME = "bitcoin-historical-data.zip"
 
-    """
+RAW_PATH = "data/raw/"
+RAW_NAME = "btcusd_1-min_data.csv"
 
-    def __init__(self, test_mode=False):
-        """
-        constructor.
-        If test_case is true, this will use different folders so unit tests don't interfere with any downloaded data.
-        """
-
-        self.DATASET_DIR_PATH = "data/raw/"
-        self.ZIP_PATH = "data/zip/"
-        self.ZIP_NAME = "bitcoin-historical-data.zip"
-
-        if test_mode:
-            self.DATASET_DIR_PATH = "tests/dataset/"
-            self.ZIP_PATH = "tests/"
-
-    def download(self) -> None:
-        """download the dataset from Kaggle and unzip it to data/dataset"""
-
-        api = KaggleApi()
-        api.authenticate()
-        api.dataset_download_files(
-            "mczielinski/bitcoin-historical-data", path=self.ZIP_PATH
-        )
-
-        zip_file = ZipFile(self.ZIP_PATH + self.ZIP_NAME)
-        zip_file = zip_file.extractall(path=self.DATASET_DIR_PATH)
-
-        os.remove(self.ZIP_PATH + self.ZIP_NAME)
-
-    def is_downloaded(self) -> bool:
-        """checks if the data already has been downloaded"""
-
-        return os.path.exists(self.DATASET_DIR_PATH + "btcusd_1-min_data.csv")
-
-    def remove_all(self) -> None:
-        """removes all files associated with this dataset from the computer"""
-
-        if os.path.exists(self.ZIP_PATH + self.ZIP_NAME):
-            os.remove(self.ZIP_PATH + self.ZIP_NAME)
-        if os.path.exists(self.DATASET_DIR_PATH):
-            shutil.rmtree(self.DATASET_DIR_PATH)
-
-    def get_original_data(self) -> pd.DataFrame:
-        """
-        Loads the original data.csv as pandas self.
-        If the dataset is not yet loaded, it will be loaded automatically.
-        """
-
-        if not self.is_downloaded():
-            self.download()
-
-        return pd.read_csv(self.DATASET_DIR_PATH + "data.csv")
+# Validate folders exist
+os.makedirs(ZIP_PATH, exist_ok=True)
+os.makedirs(RAW_PATH, exist_ok=True)
 
 
-dataset = Dataset()
-dataset.download()
+api = KaggleApi()
+api.authenticate()
+api.dataset_download_files("mczielinski/bitcoin-historical-data", path=ZIP_PATH)
+
+# Extract zip
+zip_file = ZipFile(ZIP_PATH + ZIP_NAME)
+zip_file.extractall(path=RAW_PATH)
+
+# Remove zip
+os.remove(ZIP_PATH + ZIP_NAME)
