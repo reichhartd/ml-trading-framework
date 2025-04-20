e521f5096900a11151f0567bf00f069fc634d3b5
Daniel Reichhart <daniel.reichhart@gmx.net>
2025-04-20 20:10:52 +0200

    feat: draft

--- src/data/create_dataframe.py
+++ src/data/create_dataframe.py
@@ -5,14 +5,11 @@ from . import data_logger as logger
 
 
 def create_dataframe():
-    logger.info(f"Loading data from {PROCESSED_FILE}")
     df = pd.read_csv(PROCESSED_FILE)
 
-    logger.info("Converting timestamp and setting index")
     df["Timestamp"] = pd.to_datetime(df["Timestamp"], unit="s", utc=True)
     df.set_index("Timestamp", inplace=True)
 
-    logger.info("Dropping redundant datetime column")
     df.drop(columns=["datetime"], inplace=True)
 
     logger.info("Dataframe info:")
