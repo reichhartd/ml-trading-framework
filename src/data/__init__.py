abd5652f8aed638921dfaef0690593cde1702eaf
Daniel Reichhart <daniel.reichhart@gmx.net>
2025-04-20 19:32:14 +0200

    feat: draft

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,1 +1,14 @@
+"""
+Data module for the ML trading framework.
+"""
+
+from src.utils.logger import setup_logger
+
+# Create central data module logger
+data_logger = setup_logger("src.data")
+
+# Import and expose submodules
 from .dataset import prepare_dataset
+
+# Export public API
+__all__ = ["prepare_dataset", "data_logger"]
