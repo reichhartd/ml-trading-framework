b634ac9b9c873cc52114ce4adc49988355edfc16
Daniel Reichhart <daniel.reichhart@gmx.net>
2025-04-20 18:41:03 +0200

    feat: draft

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -2,14 +2,18 @@ import os
 from kaggle.api.kaggle_api_extended import KaggleApi
 from zipfile import ZipFile
 
+# Zip
+ZIP_PATH = "data/zip/"
+ZIP_NAME = "bitcoin-historical-data.zip"
+ZIP_FILE = os.path.join(ZIP_PATH, ZIP_NAME)
+
+# Raw
+RAW_PATH = "data/raw/"
+RAW_NAME = "btcusd_1-min_data.csv"
+RAW_FILE = os.path.join(RAW_PATH, RAW_NAME)
+
 
 def download_dataset():
-    ZIP_PATH = "data/zip/"
-    ZIP_NAME = "bitcoin-historical-data.zip"
-
-    RAW_PATH = "data/raw/"
-    RAW_NAME = "btcusd_1-min_data.csv"
-
     # Validate folders exist
     os.makedirs(ZIP_PATH, exist_ok=True)
     os.makedirs(RAW_PATH, exist_ok=True)
