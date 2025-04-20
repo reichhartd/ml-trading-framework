9dc13830e53111b1d2e131557b644bc3cd4d62ab
Daniel Reichhart <daniel.reichhart@gmx.net>
2025-04-20 21:35:30 +0200

    feat: draft

--- src/features/__init__.py
+++ src/features/__init__.py
@@ -4,9 +4,9 @@ Feature module for the ML trading framework.
 
 from src.utils.logger import setup_logger
 
-# Create central data module logger
-data_logger = setup_logger("src.features")
+# Create central feature module logger
+feature_logger = setup_logger("src.features")
 
 
 # Export public API
-__all__ = ["data_logger"]
+__all__ = ["feature_logger"]
