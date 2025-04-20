306fa510ea3c5769f8a4b79d0ccfdcc8d48b22c0
Daniel Reichhart <daniel.reichhart@gmx.net>
2025-04-20 21:37:07 +0200

    feat: draft

--- src/features/features.py
+++ src/features/features.py
@@ -1,4 +1,5 @@
 from . import feature_logger as logger
+from .generate_bull_bear_signals import generate_bull_bear_signals
 from ..data import prepare_dataset
 
 
@@ -7,7 +8,15 @@ def prepare_features():
     logger.info("Starting feature preparation")
 
     try:
-        train_data, valid_data, test_data = prepare_dataset()
+        train_data, valid_data = prepare_dataset()
+
+        # Limit to the last 100,000 entries (most recent)
+        train_data = train_data.tail(100000)
+        valid_data = valid_data.tail(100000)
+
+        generate_bull_bear_signals(train_data, verbose=True)
+        generate_bull_bear_signals(valid_data, verbose=True)
+
     except Exception as e:
         logger.error(f"Error preparing features: {e}", exc_info=True)
         raise
