7e9bce84fdec3e8ebba92e5488d1249d1a93833b
Daniel Reichhart <daniel.reichhart@gmx.net>
2025-04-20 21:46:57 +0200

    feat: draft

--- src/features/features.py
+++ src/features/features.py
@@ -1,5 +1,8 @@
 from . import feature_logger as logger
+from .calculate_technical_indicators import calculate_technical_indicators
+from .config import TRAIN_DATA_WITH_FEATURES_FILE, VALIDATION_DATA_WITH_FEATURES_FILE
 from .generate_bull_bear_signals import generate_bull_bear_signals
+from .plot_target_correlation import plot_target_correlation
 
 
 def prepare_features(train_data, valid_data):
@@ -11,6 +14,14 @@ def prepare_features(train_data, valid_data):
 
         generate_bull_bear_signals(train_data, verbose=True)
         generate_bull_bear_signals(valid_data, verbose=True)
+
+        calculate_technical_indicators(train_data)
+        calculate_technical_indicators(valid_data)
+
+        plot_target_correlation(train_data, "signal")
+
+        train_data.to_csv(TRAIN_DATA_WITH_FEATURES_FILE)
+        valid_data.to_csv(VALIDATION_DATA_WITH_FEATURES_FILE)
 
     except Exception as e:
         logger.error(f"Error preparing features: {e}", exc_info=True)
