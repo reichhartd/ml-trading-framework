d327e5d71638a9d20435c72735b10405e5271fea
Daniel Reichhart <daniel.reichhart@gmx.net>
2025-04-26 12:09:44 +0200

    feat: draft

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -9,7 +9,9 @@ data_logger = setup_logger("src.data")
 
 # Import and expose submodules
 from .main import prepare_dataset
-from .to_timestamp_csv import to_timestamp_csv
 
 # Export public API
-__all__ = ["prepare_dataset", "data_logger", "to_timestamp_csv"]
+__all__ = [
+    "prepare_dataset",
+    "data_logger",
+]
