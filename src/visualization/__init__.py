91c3bb980b0bd0fb19bfdd5c2d44b4c2c6532473
Daniel Reichhart <daniel.reichhart@gmx.net>
2025-04-26 10:50:22 +0200

    feat: draft

---
+++ src/visualization/__init__.py
@@ -0,0 +1,16 @@
+"""
+Visualization module for the ML trading framework.
+"""
+
+from src.utils.logger import setup_logger
+
+# Create central feature module logger
+feature_logger = setup_logger("src.visualization")
+
+
+# Import and expose submodules
+from .plot_line import plot_line
+from .plot_target_correlation import plot_target_correlation
+
+# Export public API
+__all__ = ["plot_line", "plot_target_correlation"]
