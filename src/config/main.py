d1029526456614db2d47bf6b0ef8f027928a4950
Daniel Reichhart <daniel.reichhart@gmx.net>
2025-04-26 10:52:13 +0200

    feat: draft

---
+++ src/config/main.py
@@ -0,0 +1,29 @@
+import os
+
+
+# Zip
+ZIP_PATH = "data/zip/"
+ZIP_NAME = "bitcoin-historical-data.zip"
+ZIP_FILE = os.path.join(ZIP_PATH, ZIP_NAME)
+
+# Raw
+RAW_PATH = "data/raw/"
+RAW_NAME = "btcusd_1-min_data.csv"
+RAW_FILE = os.path.join(RAW_PATH, RAW_NAME)
+
+# Processed
+PROCESSED_PATH = "data/processed/"
+PROCESSED_NAME = "btcusd_1-min_data_processed.csv"
+PROCESSED_FILE = os.path.join(PROCESSED_PATH, PROCESSED_NAME)
+
+# Training Set (60%)
+TRAIN_DATA_NAME = "train_data.csv"
+TRAIN_DATA_FILE = os.path.join(PROCESSED_PATH, TRAIN_DATA_NAME)
+
+# Validation Set (20%)
+VALIDATION_DATA_NAME = "validation_data.csv"
+VALIDATION_DATA_FILE = os.path.join(PROCESSED_PATH, VALIDATION_DATA_NAME)
+
+# Test Set (20%)
+TEST_DATA_NAME = "test_data.csv"
+TEST_DATA_FILE = os.path.join(PROCESSED_PATH, TEST_DATA_NAME)
