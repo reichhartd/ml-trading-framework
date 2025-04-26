976e323bed498a514f3b4bdbf25d74ecf7b39971
Daniel Reichhart <daniel.reichhart@gmx.net>
2025-04-26 11:04:16 +0200

    feat: draft

--- src/features/generate_bull_bear_signals.py
+++ src/features/generate_bull_bear_signals.py
@@ -1,8 +1,8 @@
 import numpy as np
-from . import feature_logger as logger
+from ..visualization import plot_line
 
 
-def generate_bull_bear_signals(df, verbose=True):
+def generate_bull_bear_signals(df):
     # The arbitrarily chosen window values of 10 and 60 for SMA_10 and SMA_60 respectively impact performance
     # significantly and should ideally be optimized through dedicated analysis.
     df["SMA_10"] = df["Close"].rolling(window=10, min_periods=1, center=False).mean()
@@ -12,5 +12,4 @@ def generate_bull_bear_signals(df, verbose=True):
     # the strategy generates a buy signal (1); otherwise it produces a sell signal (0).
     df["signal"] = np.where(df["SMA_10"] > df["SMA_60"], 1.0, 0.0)
 
-    if verbose:
-        logger.info(df["signal"].value_counts())
+    plot_line(df, ["Close", "signal"], "Signal (Bull-Bear-Signal)")
