c9dd741e77dcbc356bcdff49bb7f38c049ec96dc
Daniel Reichhart <daniel.reichhart@gmx.net>
2025-04-26 13:58:11 +0200

    feat: draft

--- src/config/main.py
+++ src/config/main.py
@@ -39,5 +39,6 @@ TEST_DATA_NAME = "test_data.csv"
 TEST_DATA_FILE = os.path.join(PROCESSED_PATH, TEST_DATA_NAME)
 
 # Visualization
+PLOT_DATA = True
 # 8h * 60 min -> First 8 hours
 PLOT_DATA_POINTS = 8 * 60
