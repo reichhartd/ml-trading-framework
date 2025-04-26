174c44c7b64fc5b03924ba7b38495d329b1cfc36
Daniel Reichhart <daniel.reichhart@gmx.net>
2025-04-26 11:05:29 +0200

    feat: draft

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -9,7 +9,6 @@ data_logger = setup_logger("src.data")
 
 # Import and expose submodules
 from .main import prepare_dataset
-from .config import PROCESSED_PATH
 
 # Export public API
-__all__ = ["prepare_dataset", "PROCESSED_PATH", "data_logger"]
+__all__ = ["prepare_dataset", "data_logger"]
