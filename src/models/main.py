3a784a974300ee68e1df9e39b6c80882903cb06e
Daniel Reichhart <daniel.reichhart@gmx.net>
2025-04-27 18:10:12 +0200

    feat: draft

--- src/models/main.py
+++ src/models/main.py
@@ -3,18 +3,12 @@ from .evaluate_models import evaluate_models
 from ..visualization import plot_comparative_model_performance
 
 
-def prepare_models(
-    train_data_baseline, valid_data_baseline, train_data_technical, valid_data_technical
-):
+def prepare_models(train_data_baseline, valid_data_baseline, train_data_technical, valid_data_technical):
     logger.info("Starting model preparation")
     try:
 
-        baseline_results = evaluate_models(
-            train_data_baseline, valid_data_baseline, dataset_type="Baseline"
-        )
-        technical_results = evaluate_models(
-            train_data_technical, valid_data_technical, dataset_type="Technical"
-        )
+        baseline_results = evaluate_models(train_data_baseline, valid_data_baseline, dataset_type="Baseline")
+        technical_results = evaluate_models(train_data_technical, valid_data_technical, dataset_type="Technical")
 
         results_dict = {"Baseline": baseline_results, "Technical": technical_results}
 
