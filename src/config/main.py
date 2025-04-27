59eadcb54a5315247d3dbdd477e58933e3bc5014
Daniel Reichhart <daniel.reichhart@gmx.net>
2025-04-27 15:16:37 +0200

    feat: draft

--- src/config/main.py
+++ src/config/main.py
@@ -1,3 +1,18 @@
+from os import path
+
+
+# Processed
+PROCESSED_FOLDER = "data/processed/"
+TRAIN_DATA_BASELINE_FILE = path.join(PROCESSED_FOLDER, "train_data_baseline.csv")
+TRAIN_DATA_TECHNICAL_FILE = path.join(PROCESSED_FOLDER, "train_data_technical.csv")
+VALIDATION_DATA_BASELINE_FILE = path.join(
+    PROCESSED_FOLDER, "validation_data_baseline.csv"
+)
+VALIDATION_DATA_TECHNICAL_FILE = path.join(
+    PROCESSED_FOLDER, "validation_data_technical.csv"
+)
+
+
 # Visualization
 PLOT_DATA = True
 # 8h * 60 min -> First 8 hours
