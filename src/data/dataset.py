d3cae584525708c0c9922393d43649fa57ba935a
Daniel Reichhart <daniel.reichhart@gmx.net>
2025-04-27 14:47:06 +0200

    feat: draft

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,6 +1,11 @@
 from os import path, makedirs, remove
 from kaggle.api.kaggle_api_extended import KaggleApi
 from zipfile import ZipFile
+import pandas as pd
+from . import data_logger as logger
+from pytz import UTC
+from datetime import datetime
+from shutil import copy2
 
 
 class Dataset:
@@ -46,6 +51,8 @@ class Dataset:
         CSV file for the bitcoin price for the time period of 01.01.2012 10:01 until 26.04.2025 00:43.
         :return:
         """
+        logger.info("Downloading dataset")
+
         api = KaggleApi()
         api.authenticate()
         api.dataset_download_files(
@@ -58,3 +65,35 @@ class Dataset:
 
         # Remove zip
         remove(Dataset.__ZIP_FILE)
+
+    @staticmethod
+    def __process_raw_dataset():
+        # Skip if processed file already exists
+        if path.exists(Dataset.__PROCESSED_FILE):
+            df = pd.read_csv(Dataset.__PROCESSED_FILE, low_memory=False)
+            return df
+
+        # Check if raw file exists
+        if not path.exists(Dataset.__RAW_FILE):
+            Dataset.__download_dataset()
+
+        logger.info("Processing dataset")
+
+        # Function to convert Unix timestamp to formatted datetime string
+        def convert_timestamp(ts):
+            dt = datetime.fromtimestamp(float(ts), tz=UTC)
+            timezone_str = dt.strftime("%z")
+            formatted_timezone = f"{timezone_str[:3]}:{timezone_str[3:]}"
+            return dt.strftime("%Y-%m-%d %H:%M:%S") + formatted_timezone
+
+        # Copy the original file to processed directory
+        copy2(Dataset.__RAW_FILE, Dataset.__PROCESSED_FILE)
+
+        # Convert Timestamp column to datetime format
+        df = pd.read_csv(Dataset.__PROCESSED_FILE, low_memory=False)
+        df["datetime"] = df["Timestamp"].apply(convert_timestamp)
+
+        # Save the processed data
+        df.to_csv(Dataset.__PROCESSED_FILE, index=False)
+
+        return df
