c20b0106e1ee3d366aa19ab1e38a0cdd1374e9fe
Daniel Reichhart <daniel.reichhart@gmx.net>
2025-04-27 18:25:25 +0200

    feat: draft

--- src/models/evaluate_models.py
+++ src/models/evaluate_models.py
@@ -7,7 +7,6 @@ from ..visualization import plot_train_validation_data, plot_model_evaluation_re
 
 
 def evaluate_models(train_data, valid_data, target_feature="signal", dataset_type=""):
-    # Handle NaN values in the dataset
     train_data = train_data.dropna()
     valid_data = valid_data.dropna()
 
@@ -23,16 +22,13 @@ def evaluate_models(train_data, valid_data, target_feature="signal", dataset_typ
     logger.info(f"Training data shape after removing NaN values: {X_train.shape}")
     logger.info(f"Validation data shape after removing NaN values: {X_valid.shape}")
 
-    # Visualize the train/validation data
     plot_train_validation_data(train_data, valid_data, target_feature, dataset_type)
 
-    # Initialize result lists
     model_names = []
     train_results = []
     valid_results = []
     train_times = []
 
-    # Evaluate models
     for name, model in selected_models:
         model_names.append(name)
 
@@ -48,20 +44,16 @@ def evaluate_models(train_data, valid_data, target_feature="signal", dataset_typ
         train_valid_time = time.time() - train_start_time
         train_times.append(train_valid_time)
 
-        # Output results
         logger.info(f"{name} : Train: {train_result:.3f}, Valid: {valid_result:.3f} -> {train_valid_time:.2f}s")
 
-    # Summarize results in DataFrame
     results_df = pd.DataFrame(
         {
             "Training": train_results,
             "Validation": valid_results,
-            "Time (s)": train_times,
         },
         index=model_names,
     )
 
-    # Visualize model evaluation results
     plot_model_evaluation_results(results_df, dataset_type)
 
     return results_df
